f7cb225aa53ea2d8fdfcf0b4296f29357881daf0
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-10 10:50:13 -0500

    Se ha agregado el modelo de usuario en la app accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,1 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import UsuarioPersonalizado
 
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta(UserCreationForm):
+        
+        model = UsuarioPersonalizado
+        fields = ('nombre', 'ap_paterno', 'ap_materno', 'username', 'email')
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UsuarioPersonalizado(AbstractUser):
+
+    nombre = models.CharField(max_length=30)
+    ap_paterno = models.CharField(max_length=30)
+    ap_materno = models.CharField(max_length=30)
