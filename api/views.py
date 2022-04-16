931a25cde1e72e3cb7b9b6fcd5e1957e42428626
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-16 12:50:41 -0500

    feat(backend): views.py file in api app was modified
    GruposViewSet was added the serializer class and UserViewSet was added

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets # new
 from grupo.models import Grupo
 from .permissions import IsAuthorOrReadOnly
+from .serializers import GrupoSerializer, UserSerializer
+from django.contrib.auth import get_user_model # new
 
 
 # Create your views here.
@@ -8,3 +10,10 @@ class GruposViewSet(viewsets.ModelViewSet):
     
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Grupo.objects.all()
+    serializer_class = GrupoSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
